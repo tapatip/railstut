118a2e3ebed0aaaf75db6175485edf8e734fec53
fghd <tapatip@yandex.ru>
2014-04-15 16:39:59 +0600

    Added validation of title in Product model, the title must be at least than 10 symbols

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10, too_short: "title must be at least than 10 symbols"}
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|png|jpg)\Z}i,
       message: 'URL должен указывать на изображение формата GIF, PNG или JPG'
